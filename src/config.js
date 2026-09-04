525572a1c35307a37f7aa3e35990e191ffb2374e
Reansykes <85455939+reansky@users.noreply.github.com>
2026-09-04 19:46:27 +0700

    Update chartUrl to new GeckoTerminal link

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const config = {
   contractUrl: "https://arbiscan.io/token/0xc117e2cec8de43c570b2f0c0a2fd5b24604fcba3",
   tax: "0%",
   buyUrl: "https://bankr.bot/terminal/trade?out=0xc117e2cec8de43c570b2f0c0a2fd5b24604fcba3&chain=arbitrum",
-  chartUrl: "https://www.geckoterminal.com/robinhood/pools/0x5199aC816bB324643c2195831eE835CcEaAAC2D0",
+  chartUrl: "https://www.geckoterminal.com/arbitrum/pools/0x0fb8e296e1a9066e944cc3b5cb209bc4c98e69b5fb460d3d9a4264570e475b1b",
   xUrl: "https://x.com/arbitrumwojak",
   arbitrumUrl: "https://arbitrum.io/",
 };
